f472b7652ffc07ea6bbcd6920111b74ac11f007f
xDark <19853368+xxDark@users.noreply.github.com>
2020-11-01 20:37:28 +0300

    Allow to build fat jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,14 +12,29 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.netty:netty-all:4.1.53.Final")
+    compileOnly("io.netty:netty-all:4.1.53.Final")
 }
 
 tasks {
+    withType<Jar> {
+        // Otherwise you'll get a "No main manifest attribute" error
+        manifest {
+            attributes["Agent-Class"] = "dev.xdark.nettyctl.NettyctlKt"
+        }
+    }
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    register<Jar>("fatJar") {
+        baseName = "${project.name}-fat"
+        from(
+            configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
+            exclude("META-INF/")
+        }
+
+        with(get("jar") as CopySpec)
     }
 }
